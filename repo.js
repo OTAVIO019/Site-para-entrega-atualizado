6cf0bb99d0e8cae0a93b1e70e242028462c0b34f
Otávio <oamsouza@sga.pucminas.br>
2024-06-26 22:17:43 -0300

    FÉ EM DEUS QUE ELE É JUSTO

---
+++ repo.js
@@ -0,0 +1,31 @@
+const repositories = document.querySelector('.content-main')
+
+function getApiGitHub(){
+    fetch('https://api.github.com/users/OTAVIO019/repos')
+    .then(async res=>{
+        if( !res.ok){
+            throw new console.error(res.status);
+        }
+        let data = await res.json();
+        data.map(item=>{
+            let project = document.createElement('div');
+            project.innerHTML= `
+        <div class="project">
+                <div>
+                    <h4 class="title">${item.name}</h4>
+                    <span class="date-create">${Intl.DateTimeFormat('pt-br').format(new Date(item.created_at))}</span>
+                </div>
+                <div>
+                    <a href="${item.html_url}">${item.html_url}</a>
+                    <span class="linguagem"><span class="circle"></span>${item.language}</span>
+
+                </div>
+        </div>
+            `
+            repositorio.appendChild(project)
+        })
+
+    })
+        
+}
+getApigithub()
